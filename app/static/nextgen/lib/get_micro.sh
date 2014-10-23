9a2632945b016d97a765e25d709fc50d34dfef97
Jim Smith <jim.smith@vibrantmedia.com>
2014-10-23 15:15:25 +0100

    Removed old demos

--- app/static/nextgen/lib/get_micro.sh
+++
@@ -1,2 +0,0 @@
-cp /Users/jim/Dropbox/Public/micro/micro.js .
-cp /Users/jim/Dropbox/Public/micro/plushness.js .
--- app/static/nextgen/lib/plushness.js
+++
@@ -1,5 +0,0 @@
-function iteration(list, func) {
-	for (var i = 0; i < list.length; i++) {
-		func(list[i]);
-	}
-}
